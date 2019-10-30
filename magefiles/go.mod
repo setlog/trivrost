27b04b0f53fca1526c7f88ada9eeb24419a3b5f0
Martin Multhaupt <m.multhaupt@setlog.com>
2019-10-30 15:05:14 +0100

    Experimental magefile

---
+++ magefiles/go.mod
@@ -0,0 +1,8 @@
+module github.com/setlog/trivrost/mage
+
+require (
+	github.com/codeskyblue/go-sh v0.0.0-20190412065543-76bd3d59ff27
+	github.com/setlog/trivrost v1.3.4 // indirect
+)
+
+go 1.13
---
+++ magefiles/magefile.go
@@ -0,0 +1,30 @@
+//+build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	sh "github.com/codeskyblue/go-sh"
+)
+
+func Generate() error {
+	s := sh.NewSession()
+	s.SetEnv("GO111MODULE", "off")
+	if err := s.Command("go", "get", "-u", "github.com/josephspurrier/goversioninfo/cmd/goversioninfo").Run(); err != nil {
+		return fmt.Errorf("could not go get goversioninfo: %w", err)
+	}
+	s = sh.NewSession()
+	s.SetDir(filepath.Dir(wd()))
+	return s.Command("go", "generate", "-installsuffix", "_separate", "github.com/setlog/trivrost/cmd/launcher").Run()
+}
+
+func wd() string {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return workingDirectory
+}
